05aadbf864f3c7fe3d033d65684d40bc2a5e97ea
Nikhil Rana <harrybite29@gmail.com>
2024-09-04 11:47:06 +0530

    added new store for unique key

--- x/identity/keeper/id.go
+++ x/identity/keeper/id.go
@@ -18,33 +18,33 @@ func (k Keeper) SetId(ctx context.Context, id types.Id) {
 	b := k.cdc.MustMarshal(&id)
 
 	uniquekey := types.IdKey(id.Did + ":" + id.Username)
-
 	store.Set(uniquekey, b)
 
-	store.Set(types.IdKey(id.Did), uniquekey)
-
-	store.Set(types.IdKey(id.Creator), uniquekey)
-
-	store.Set(types.IdKey(id.Username), uniquekey)
+	uniquestore := prefix.NewStore(storeAdapter, types.KeyPrefix(types.UniqueIdKeyPrefix))
+	uniquestore.Set(types.IdKey(id.Did), uniquekey)
+	uniquestore.Set(types.IdKey(id.Creator), uniquekey)
+	uniquestore.Set(types.IdKey(id.Username), uniquekey)
 }
 
 // update the current user and delete the olduser uniquekey
 func (k Keeper) UpdateUserId(ctx context.Context, id types.Id, oldCreator string) {
 	storeAdapter := runtime.KVStoreAdapter(k.storeService.OpenKVStore(ctx))
 	store := prefix.NewStore(storeAdapter, types.KeyPrefix(types.IdKeyPrefix))
+	uniquestore := prefix.NewStore(storeAdapter, types.KeyPrefix(types.UniqueIdKeyPrefix))
+
 	b := k.cdc.MustMarshal(&id)
 
 	uniquekey := types.IdKey(id.Did + ":" + id.Username)
 
-	store.Delete(types.IdKey(oldCreator))
+	uniquestore.Delete(types.IdKey(oldCreator))
 
 	store.Set(uniquekey, b)
 
-	store.Set(types.IdKey(id.Did), uniquekey)
+	uniquestore.Set(types.IdKey(id.Did), uniquekey)
 
-	store.Set(types.IdKey(id.Creator), uniquekey)
+	uniquestore.Set(types.IdKey(id.Creator), uniquekey)
 
-	store.Set(types.IdKey(id.Username), uniquekey)
+	uniquestore.Set(types.IdKey(id.Username), uniquekey)
 
 }
 
@@ -56,15 +56,16 @@ func (k Keeper) GetId(
 ) (val types.Id, found bool) {
 	storeAdapter := runtime.KVStoreAdapter(k.storeService.OpenKVStore(ctx))
 	store := prefix.NewStore(storeAdapter, types.KeyPrefix(types.IdKeyPrefix))
+	uniquestore := prefix.NewStore(storeAdapter, types.KeyPrefix(types.UniqueIdKeyPrefix))
 
-	b := store.Get(types.IdKey(
-		did,
-	))
-	if b == nil {
+	uniquekey := uniquestore.Get(types.IdKey(did))
+	if uniquekey == nil {
 		return val, false
 	}
 
-	k.cdc.MustUnmarshal(b, &val)
+	data := store.Get(uniquekey)
+
+	k.cdc.MustUnmarshal(data, &val)
 	return val, true
 }
 
@@ -74,13 +75,14 @@ func (k Keeper) GetIdByDidorUsernameorCreator(
 
 ) (val types.Id, found bool) {
 	storeAdapter := runtime.KVStoreAdapter(k.storeService.OpenKVStore(ctx))
-	store := prefix.NewStore(storeAdapter, types.KeyPrefix(types.IdKeyPrefix))
+	uniquestore := prefix.NewStore(storeAdapter, types.KeyPrefix(types.UniqueIdKeyPrefix))
 
-	uniquekey := store.Get(types.IdKey(key))
+	uniquekey := uniquestore.Get(types.IdKey(key))
 	if uniquekey == nil {
 		return val, false
 	}
 
+	store := prefix.NewStore(storeAdapter, types.KeyPrefix(types.IdKeyPrefix))
 	data := store.Get(uniquekey)
 
 	k.cdc.MustUnmarshal(data, &val)
